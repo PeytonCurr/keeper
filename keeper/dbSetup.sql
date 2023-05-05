812e4bb517148caa9cff2140c893297d1834691a
peyton <peyton.curr@gmail.com>
2023-05-05 11:15:38 -0600

    Created keeps Table in SQL

--- keeper/dbSetup.sql
+++ keeper/dbSetup.sql
@@ -7,3 +7,15 @@ CREATE TABLE
         email varchar(255) COMMENT 'User Email',
         picture varchar(255) COMMENT 'User Picture'
     ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS keeps(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(50) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        img VARCHAR(255) NOT NULL,
+        views BIGINT,
+        kept INT,
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8mb4 COMMENT '';
